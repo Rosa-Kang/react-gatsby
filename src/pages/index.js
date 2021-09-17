6d09c610a02a330af0f5fc3f33fa6afe3b0e9bd6
Rosa Kang <therosessom@gmail.com>
2021-09-17 13:19:00 -0700

    update gatsby version 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Seo from "../components/Seo"
 import Trips from "../components/Trips"
 import Testimonials from "../components/Testimonials"
 import Stats  from "../components/Stats"
+import Email from "../components/Email"
 
 const IndexPage = () => (
   <Layout>
@@ -12,7 +13,8 @@ const IndexPage = () => (
     <Hero />
     <Trips heading="Our Favorite Destinations" />
     <Testimonials />
-    <Stats/>
+    <Stats />
+    <Email />
   </Layout>
 )
 
